22a2654164e66234597500faa30e2b7d75f8da44
Jin Wu <jinn.wuu@gmail.com>
2020-04-06 12:07:40 -0400

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-user1 = User.create(username: "asdf", password: "asdf", first_name: "Apple", last_name: "Pie", cash: "1000")
+user1 = User.create(username: "asdf", password: "asdf", first_name: "Mc", last_name: "Donalds", cash: "1000")
+user2 = User.create(username: "qwerty", password: "asdf", first_name: "Burger", last_name: "King", cash: "1000")
 
-portfolio1 = Portfolio.create(description: "Random Portfolio", user_id: user1.id)
+portfolio1 = Portfolio.create(description: "Tech Stocks", user_id: user1.id)
+portfolio2 = Portfolio.create(description: "Random Stocks", user_id: user2.id)
 
 stock1 = Stock.create(symbol: "AAPL", name: "Apple Inc.", price: "241.41", exchange: "Nasdaq Global Select", portfolio_id: portfolio1.id)
 stock2 = Stock.create(symbol: "MSFT", name: "Microsoft Corporation", price: "153.83", exchange: "Nasdaq Global Select", portfolio_id: portfolio1.id)
+stock3 = Stock.create(symbol: "SNAP", name: "Snap Inc. Class A", price: "11.475", exchange: "New York Stock Exchange", portfolio_id: portfolio2.id)
+stock4 = Stock.create(symbol: "WFC", name: "Wells Fargo & Company", price: "27.83", exchange: "New York Stock Exchange", portfolio_id: portfolio2.id)
+stock5 = Stock.create(symbol: "NKE", name: "Nike Inc.", price: "82.62", exchange: "New York Stock Exchange", portfolio_id: portfolio2.id)
 
 puts "Done"
