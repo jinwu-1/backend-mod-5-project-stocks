3c401511b3687f0d0ace9e040dddf34935fcbfbe
Jin Wu <jinn.wuu@gmail.com>
2020-04-07 16:54:20 -0400

    can update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,12 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        render json: @user
+    end
+
     private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :stocks, only: [:index, :create, :update, :destroy]
-  resources :users, only: [:index, :create]
+  resources :users, only: [:index, :create, :update]
   post '/login', to: 'users#login'
   get '/persist', to: 'users#persist'
 end
