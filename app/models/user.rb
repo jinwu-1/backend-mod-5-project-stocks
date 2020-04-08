160b69dd3c1bd6140db2096865ef5e2888325919
Jin Wu <jinn.wuu@gmail.com>
2020-04-08 10:16:19 -0400

    fixed update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
     validates :username, uniqueness: { case_sensitive: false}
     validates :username, length: { minimum: 4 }
-    validates :password, length: { minimum: 4 }
+    # validates :password, length: { minimum: 3 }
     
     has_secure_password
 end
