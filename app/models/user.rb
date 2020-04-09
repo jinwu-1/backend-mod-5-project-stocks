296c153828506474cf219f97374661489bd261ac
Jin Wu <jinn.wuu@gmail.com>
2020-04-09 15:34:31 -0400

    new validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
     has_many :stocks
 
-    validates :username, uniqueness: { case_sensitive: false}
+    validates :username, uniqueness: { case_sensitive: false }
+
     validates :username, length: { minimum: 4 }
     # validates :password, length: { minimum: 3 }
+    validate :minimum_cash
     
     has_secure_password
+
+    def minimum_cash
+        user_cash = self.cash.to_f.round(2)
+        if user_cash < 0
+            errors.add(:cash, "not enough money")
+        end
+    end
+
 end
